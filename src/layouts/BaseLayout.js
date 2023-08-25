9c333360efe105f1ed32e16cc81bf35a152cdb9d
Manuel Sturla <manuelsturla@gmail.com>
2023-08-25 18:27:41 -0300

    Make mobile screen use less width for containerr

--- src/layouts/BaseLayout.js
+++ src/layouts/BaseLayout.js
@@ -8,25 +8,26 @@ import { Container } from "@mui/material";
 import DefaultNavbar from "components/Navbars/DefaultNavbar";
 import CenteredFooter from "components/Footers/CenteredFooter";
 import MKBox from "components/MKBox";
-
+import useMediaQuery from "@mui/material/useMediaQuery";
 // Routes
 import { Outlet } from "react-router-dom";
 import navbarRoutes from "routes/navbarRoutes";
 
 export default function BaseLayout() {
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
   return (
     <MKBox display="flex" flexDirection="column" variant="gradient" bgColor="light" minHeight="100vh">
       <MKBox bgColor="white" shadow="sm" py={0.25}>
         <DefaultNavbar routes={navbarRoutes} action={false} transparent relative center />
       </MKBox>
-      <Container>
+      <Container disableGutters={isSmallScreen}>
         <Grid
           flexDirection="column"
           justifyContent="center"
           sx={{
             // p: 2,
             // mx: { xs: 2, lg: 3 },
-            my: 5,
+            my: { xs: 0, sm: 5 },
           }}>
           <Outlet />
         </Grid>
