e1538d04778e8fcf255231e033f94cff1d5c8ce9
Manuel Sturla <manuelsturla@gmail.com>
2023-10-26 20:29:06 -0300

    fix mobile search "Todos" option

--- src/components/Tables/FilterComponents/MobileSelectFilter.js
+++ src/components/Tables/FilterComponents/MobileSelectFilter.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 // Components
 import MKInput from "components/MKInput";
 import MenuItem from "@mui/material/MenuItem";
+import MKTypography from "components/MKTypography";
 
 MobileSelectFilter.propTypes = {
   column: PropTypes.shape({
@@ -26,7 +27,11 @@ export default function MobileSelectFilter({ column, filterValue, setFilter }) {
 
   return (
     <MKInput select label={column.header} value={filterValue || ""} onChange={handleChange} sx={{ width: "100%" }}>
-      <MenuItem value={""}>&nbsp;</MenuItem>
+      <MenuItem value={""}>
+        {/* <MKTypography variant="body2" sx={{ fontStyle: "italic", color: "grey.600" }} > */}
+        Todos
+        {/* </MKTypography> */}
+      </MenuItem>
       {column.filterSelectOptions?.map((option) => (
         <MenuItem key={option.value} value={option.value}>
           {option.text}
