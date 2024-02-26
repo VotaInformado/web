ca9efa0642532e435bea8fdd9c4918e5fae833f6
Manuel Sturla <manuelsturla@gmail.com>
2024-02-26 17:22:50 -0300

    fix error with linter and add text if copyToClipboard errors

--- src/components/CopyToClipboardButton.js
+++ src/components/CopyToClipboardButton.js
@@ -4,6 +4,7 @@ import propTypes from "prop-types";
 
 // Components
 import { Tooltip, IconButton, Icon } from "@mui/material";
+import { toast } from "react-toastify";
 
 CopyToClipboardButton.propTypes = {
   value: propTypes.string.isRequired,
@@ -13,8 +14,14 @@ export default function CopyToClipboardButton({ value }) {
   const [open, setOpen] = useState(false);
 
   function copyToClipboard() {
-    navigator.clipboard.writeText(value);
-    setOpen(true);
+    navigator.clipboard
+      .writeText(value)
+      .then(() => {
+        setOpen(true);
+      })
+      .catch((err) => {
+        toast.error("No se pudo copiar");
+      });
   }
 
   useEffect(() => {
@@ -27,13 +34,7 @@ export default function CopyToClipboardButton({ value }) {
   }, [open]);
 
   return (
-    <Tooltip
-      open={open}
-      title="Copiado!"
-      placement="bottom"
-      enterTouchDelay={0}
-      leaveTouchDelay={5000}
-      onClick={copyToClipboard}>
+    <Tooltip open={open} title="Copiado!" placement="bottom" enterTouchDelay={0} leaveTouchDelay={5000}>
       <IconButton size="small" onClick={copyToClipboard}>
         <Icon color="secondary">content_copy_icon </Icon>
       </IconButton>
